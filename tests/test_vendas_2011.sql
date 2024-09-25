94566ae0c34ce5845ce8210490778249429a7dce
Bruna Praça <bruna.praca@indicium.tech>
2024-09-25 00:25:22 +0000

    ajuste na tabela de teste

--- tests/test_vendas_2011.sql
+++ tests/test_vendas_2011.sql
@@ -1,7 +1,14 @@
  /* Teste de vendas brutas no ano de 2011 - Auditado por Carlos 
- Total = $12.646.112,16 */
+ Total = $12.646.112,1607 */
 
-select count(*)
-from {{ ref('vendas_2011') }}
+with vendas_2011 as (
+    select 
+        SUM(preco_unitario * quantidade_comprada) as vendas_brutas
+    from {{ ref('int_sales') }}
+    where data_pedido between '2011-01-01' and '2011-12-31'
+)
+
+select
+    vendas_brutas
+from vendas_2011
 where vendas_brutas <> 12646112.1607
-
